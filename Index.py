bd1747a16784be21ec589a626b62d3acf58eb5ef
mukulsaini0001 <mukulsaini548@gmail.com>
2023-09-06 12:09:48 +0530

    added comments

--- Index.py
+++ Index.py
@@ -2,11 +2,11 @@ import psutil
 import time
 
 def measure_power_consumption(application_name, duration_seconds):
-    pid = None
-    for process in psutil.process_iter(attrs=['pid', 'name']):
-        if process.info['name'] == application_name:
+    pid = None  #initialize pid to None
+    for process in psutil.process_iter(attrs=['pid', 'name']): #Iterate through the list of running 
+        if process.info['name'] == application_name: #iteration, check if its
             pid = process.info['pid']
-            break
+            break 
 
     if pid is None:
         print(f"Application '{application_name}' not found.")
@@ -26,20 +26,17 @@ def measure_power_consumption(application_name, duration_seconds):
     delta_cpu = after_cpu_usage - before_cpu_usage
     delta_memory = after_memory_usage - before_memory_usage
 
+    #changes in CPU and memory usage percentages during the monitoring period
     print(f"Estimated power consumption of '{application_name}':")
     print(f"CPU Usage: {delta_cpu}%")
     print(f"Memory Usage: {delta_memory}%")
 
-
+#target application by displaying the changes in CPU and memory usage percentages
 if __name__ == "__main__":
-    target_applications = input("Enter target applications: ").split(',')
+    target_applications = input("Enter target applications: ").split(',')[0]+".exe"
     measurement_duration_seconds = int(input("Enter measurement duration in seconds: "))
-    target_applications = [app.strip() for app in target_applications]  
+    measure_power_consumption(target_applications, measurement_duration_seconds) 
 
-    
-    for app_name in target_applications:
-        app_name = app_name.strip()  
-        measure_power_consumption(app_name, measurement_duration_seconds) 
 
 
 
